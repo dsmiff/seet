f29a8c2f6fe416714fbab2f5fb8d853b1bd06cb6
Dominic Smith <dosmith@cern.ch>
2016-12-22 22:02:00 +0000

    Encore des documents

--- js/blog.js
+++ js/blog.js
@@ -28,7 +28,9 @@ function gitHub() {
     document.getElementById("blog_v2").innerHTML = "Aim is to easily retrieve pull requests made to the repository's base branch";
 }
 
-document.getElementById("blog_v2_title").innerHTML = 'Test Title <span style="float: right"> <h3 class="blog_v2_date"> 22nd December 2016</h3> </span>'
-document.getElementById("blog_v2").innerHTML = 'Test'
-
-
+// JavaScript
+document.getElementById("blog_v2_title").innerHTML = 'Testing JavaScript <span style="float: right"> <h3 class="blog_v2_date"> 22nd December 2016</h3> </span>'
+var text_v2 = document.getElementById("blog_v2");
+text_v2.innerHTML = '<p>Used primarily in web development, JavaScript has been designed to allow programmatical control over the objects in its evironment</p>\n<p>fsdfsdf</p>';
+text_v2.innerHTML += '<p> Unlike Python or R, where one can load up and run code via the command line, with JavaScript the faff of creating an HTML document to load via a browser gets a bit annoying. </p>'
+text_v2.innerHTML += '<p> Fortunately, there exists an even quicker way of getting to grips with JavaScript. Just go to <a href="http://jsbin.com/hetonaxoha/edit?html,output">JSBin </a> and have a bash at running your snippets </p>'
